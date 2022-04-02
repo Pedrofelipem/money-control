1ea324fa8d188a4602b924e34bc1ab7b90c9688c
Pedrofelipem <felipexexeu02@gmail.com>
2022-04-02 16:27:17 -0300

    Components: header - building application header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import logoImg from '../../assets/logo.svg'
+import { Container, Content, LogoTipo } from './styles'
+
+export function Header(){
+    return(
+        <Container>
+            <Content>
+                <LogoTipo>
+                    <img src={logoImg} alt="Money control" />
+                    <h3>Money control</h3>
+                </LogoTipo>
+                <button type="button"> Nova transação</button>
+            </Content>
+        </Container>
+    )
+}
