0cda21bdf2a3419b94fcffcd5992b72aec2db614
Pedrofelipem <felipexexeu02@gmail.com>
2022-04-02 15:35:51 -0300

    Styles: configuring global fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,7 +2,7 @@ import {createGlobalStyle} from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
     :root{
-        --background: #f0f2f5;
+        --background: #F0F2F5;
         --red: #E52E40;
         --blue: #5429CC;
 
@@ -33,6 +33,18 @@ export const GlobalStyle = createGlobalStyle`
     body{
         background-color: var(--background);
         -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
+    }
+
+    button {
+        cursor: pointer;
     }
 
     [disabled] {
