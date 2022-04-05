042c4bc99fe0f53407506fcf3b868ca93d1a214d
Pedrofelipem <felipexexeu02@gmail.com>
2022-04-05 16:53:05 -0300

    NewTransactionModal: modal - colors on buttons

--- src/components/NewTransactionModal/newTransactionModal.tsx
+++ src/components/NewTransactionModal/newTransactionModal.tsx
@@ -13,7 +13,7 @@ interface NewTransactionModalProps{
 export function NewTransactionModal({isOpen,  onRequestClose}: NewTransactionModalProps){
 
     const [type, setType] = useState('deposit');
-    
+
     return(
         <Modal
             isOpen={isOpen}
@@ -39,6 +39,7 @@ export function NewTransactionModal({isOpen,  onRequestClose}: NewTransactionMod
                         type="button"
                         onClick={() => { setType('deposit'); }}
                         isActive={ type === "deposit"}
+                        activeColor="green"
                     > 
                         <img src={incomeImg} alt="Entrada" />
                         <span>Entrada</span>
@@ -48,6 +49,7 @@ export function NewTransactionModal({isOpen,  onRequestClose}: NewTransactionMod
                         type="button"
                         onClick={() => { setType('withdraw'); }}
                         isActive={ type === "withdraw"}
+                        activeColor="red"
                     > 
                         <img src={outcomeImg} alt="Saída" />
                         <span>Saída</span>
--- src/components/NewTransactionModal/styles-newTransactionModal.ts
+++ src/components/NewTransactionModal/styles-newTransactionModal.ts
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
-
-import {darken} from 'polished';
+import {darken, transparentize} from 'polished';
 
 export const Container = styled.form`
     h2 {
@@ -58,6 +57,12 @@ export const TransactionTypeContainer = styled.div`
 `;
 interface RadioBoxProps {
     isActive: boolean;
+    activeColor: 'green' | 'red';
+}
+
+const colors = {
+    green : '#33CC95',
+    red : '#E52E40'
 }
 
 export const RadioBox = styled.button<RadioBoxProps>`
@@ -65,7 +70,7 @@ export const RadioBox = styled.button<RadioBoxProps>`
         border: 1px solid #d7d7d7;
         border-radius: 0.25rem;
 
-        background: ${ (props) => props.isActive ? '#ccc' : 'transparent' };
+        background: ${ (props) => props.isActive ? transparentize(0.9, colors[props.activeColor]) : 'transparent' };
 
         display: flex;
         align-items: center;
