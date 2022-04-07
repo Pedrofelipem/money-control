eaf8016f58f5e4fe1102dea0f1a7ee56a51060cd
Pedrofelipem <felipexexeu02@gmail.com>
2022-04-07 15:54:48 -0300

    NewTransactionModal: Inserting transactions into the API

--- src/components/NewTransactionModal/newTransactionModal.tsx
+++ src/components/NewTransactionModal/newTransactionModal.tsx
@@ -3,6 +3,7 @@ import Modal from 'react-modal'
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
+import { api } from '../../providers/api';
 
 import { Container, RadioBox, TransactionTypeContainer } from './styles-newTransactionModal';
 interface NewTransactionModalProps{
@@ -11,6 +12,7 @@ interface NewTransactionModalProps{
 }
 
 export function NewTransactionModal({isOpen,  onRequestClose}: NewTransactionModalProps){
+    
     const [title, setTitle] =  useState('');
     const [value, setValue] =  useState(0);
     const [category, setCategory] =  useState('');
@@ -19,12 +21,15 @@ export function NewTransactionModal({isOpen,  onRequestClose}: NewTransactionMod
     function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
 
-        console.log({
+        const data = {
             title,
             value,
             category,
             type
-        })
+        };
+
+        api.post('transactions', data)
+
     }
 
     return(
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 import { App } from './App';
 
+
 createServer({
-  routes(){
+  models: {
+    transacion: Model,
+  },
+
+  routes() {
+
     this.namespace = 'api';
+
     this.get('transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'food',
-          createdAt: new Date()
-        }
-      ]
+
+      return this.schema.all('transaction')
+
+    })
+    this.post('/transactions', (schema, request) => {
+
+      const data = JSON.parse(request.requestBody)
+      return schema.create('transaction', data)
+      
     })
   }
 })
