91e3ed3887e397595edc084de235109c1611ae81
Pedrofelipem <felipexexeu02@gmail.com>
2022-04-09 14:44:17 -0300

    TrasactionsTable: trasactionsTable - formatting values

--- src/components/transactionsTable/styles-transactionsTable.ts
+++ src/components/transactionsTable/styles-transactionsTable.ts
@@ -30,7 +30,7 @@ export const Container = styled.div`
                 color: var(--green);
             }
 
-            &.whithdraw {
+            &.whithDraw {
                 color: var(--red);
             }
         }  
--- src/components/transactionsTable/transactionsTable.tsx
+++ src/components/transactionsTable/transactionsTable.tsx
@@ -38,9 +38,17 @@ export function TransactionsTable(){
                 return( 
                   <tr key={transaction.id}>
                     <td>{transaction.title}</td>
-                    <td className={transaction.type}>{transaction.amount}</td>
+                    <td className={transaction.type}>
+                      {new Intl.NumberFormat('pt-br', {
+                        style: 'currency',
+                        currency: 'BRL'
+                      }).format(transaction.amount)}</td>
                     <td>{transaction.category}</td>
-                    <td>{transaction.createdAt}</td>
+                    <td> 
+                      {new Intl.DateTimeFormat('pt-br').format(
+                        new Date(transaction.createdAt)
+                      )}
+                    </td>
                   </tr>
                 );
               })} 
