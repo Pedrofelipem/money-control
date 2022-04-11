c0e63fa901e6503ab78880caace4151a5133aff4
Pedrofelipem <felipexexeu02@gmail.com>
2022-04-11 19:15:35 -0300

    Completing application

--- src/components/NewTransactionModal/newTransactionModal.tsx
+++ src/components/NewTransactionModal/newTransactionModal.tsx
@@ -1,9 +1,9 @@
-import { FormEvent, useContext, useState } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal'
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
-import { TransactionsContext } from '../../TransactionsContext';
+import { useTransactions } from '../../hooks/useTransactions';
 
 import { Container, RadioBox, TransactionTypeContainer } from './styles-newTransactionModal';
 interface NewTransactionModalProps{
@@ -13,7 +13,7 @@ interface NewTransactionModalProps{
 
 export function NewTransactionModal({isOpen,  onRequestClose}: NewTransactionModalProps){
 
-    const { createTransaction } = useContext(TransactionsContext);
+    const { createTransaction } = useTransactions();
 
     const [title, setTitle] =  useState('');
     const [amount, setAmount] =  useState(0);
--- src/components/Summary/summary.tsx
+++ src/components/Summary/summary.tsx
@@ -1,14 +1,14 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import income from '../../assets/income.svg';
 import outcome from '../../assets/outcome.svg';
 import total from '../../assets/total.svg'
-import { TransactionsContext } from '../../TransactionsContext';
+import { useTransactions } from '../../hooks/useTransactions';
 
 import { Container } from './styles-summary';
 
 export function Summary(){ 
     
-    const {transactions} = useContext(TransactionsContext);
+    const {transactions} = useTransactions();
     
     const summary = transactions.reduce((acc, transaction) =>{
         if (transaction.type === 'deposit'){
